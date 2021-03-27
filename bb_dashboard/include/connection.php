477235659ff543432413ffbd4051057584c92c88
Yogesh Karande <yogeshkarande640@gmail.com>
2021-03-27 13:21:51 +0530

    bbms remote heroku

---
+++ bb_dashboard/include/connection.php
@@ -0,0 +1,13 @@
+<?php
+/*
+	define("HOSTNAME","localhost");
+	define("USERNAME","root");
+	define("PASSWORD","");
+	define("DBNAME","registerdonor");
+	$conn = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DBNAME) or die("cannot connect to database");
+*/
+
+// $dbh = new PDO("mysql:host=localhost;dbname=registerdonor", "root", "");
+$dbh = new PDO("mysql:host=remotemysql.com;dbname=zuwjKHVjka", "zuwjKHVjka", "9cB5tVDRJy");
+
+?>
